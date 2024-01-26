f9b7bfb8e2de743add628361e476b41889e4eb83
Astha <ashutosh1102.edu.in>
2024-01-27 01:19:09 +0530

    added some social media icon in login page

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import Login from "./components/Login/Login";
 function App() {
   return (
     <div className="bg-black">
-    {/* <h1 className="text-7xl text-center text-blue-400">Astha</h1> */}
-    {/* <div className='flex'>
-       <div className="h-screen  bg-blue-600 ">Astha</div>
-       <div className="h-screen bg- bg-white" > Singh</div> 
-    </div> */}
+    
     <Login/>
     </div>
   );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,41 @@
 import React from "react";
 import Signin from "./Signin/Signin";
 import logo from "../../Assets/Ellipse.png";
+import github from "../../Assets/github.png";
+import twitter from "../../Assets/twitter.png";
+import linkedin from "../../Assets/linkedin.png";
+import discord from "../../Assets/discord.png";
+import Vector from "../../Assets/Vector 7.png";
+
+
+
+
 
 export default function Login() {
   return (
-    <div className="flex">
-      <div className="h-screen w-1/2 bg-[#605BFF]  ">
-         <div>
-          <img src={logo} alt="Google Icon" className="w-10 h-10 mr-2" />
+    <div className="flex " >
+      <div className="h-screen w-1/2 bg-[#605BFF] flex flex-col justify-between items-center p-5 relative">
+        <div className="w-full  relative">
+        <img src={logo} alt="Logo" className="w-10 h-10 mr-2" />
+    <img src={Vector} alt="Vector" className="w-10 h-8 absolute top-0 left-0" />
+
         </div>
         <div className="text-white text-5xl font-bold">BASE</div>
+         <div className=" flex m-4 ">
+            <div className="mr-3">
+                <img src={github} alt="Github Icon" className="w-5 h-5 mr-2"></img>
+            </div>
+            <div className="mr-3">
+            <img src={twitter} alt="Github Icon" className="w-5 h-5 mr-2"></img>
+            </div>
+            <div className="mr-3">
+            <img src={linkedin} alt="Github Icon" className="w-5 h-5 mr-2"></img>
+            </div>
+            <div className="mr-3">
+            <img src={discord} alt="Github Icon" className="w-5 h-5 mr-2"></img>
+            </div>
+         </div>
+
       </div>
       <div className="h-screen w-1/2  bg-[#F8FAFF] flex justify-center items-center">
         <Signin />
